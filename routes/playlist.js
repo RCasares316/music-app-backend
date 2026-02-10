f961c21f3a82d15ee4a0222d379714ba14c774d5
RCasares316 <richiecasares@gmail.com>
2026-02-10 15:14:45 -0500

    routes playlist.js updted

--- routes/playlist.js
+++ routes/playlist.js
@@ -4,4 +4,12 @@ const router = Router();
 
 // routes ex. router.get("/", controllers.getPlaylists)
 
+router.get("/", controllers.gteUserPlaylist);
+router.get("/:playlistId", controllers.gteUserPlaylist);
+router.post("/", controllers.createPlaylist);
+router.put("/:playlistId", controllers.updatePlaylist);
+router.put("/:playlistId/track/:trackId", controllers.addTrack);
+router.put("/:playlistId/track/:trackId", controllers.removeTrack);
+router.delete("/:playlistId", controllers.deletePLaylist);
+
 export default router;
