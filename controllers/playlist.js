ba6f2dd4148796fb34e8a844a265a05c5863ff6f
RCasares316 <richiecasares@gmail.com>
2026-02-10 15:31:47 -0500

    updates made

--- controllers/playlist.js
+++ controllers/playlist.js
@@ -1,57 +1,50 @@
 import Playlist from "../models/playlist.js";
 
 export const getUserPlaylists = (req, res) => {
-try {
-    
-} catch (error) {
-    
-}
-}
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const getUserPlaylist = (req, res) => {
-try {
-    
-} catch (error) {
-    
-}
-}
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const createPlaylist = (req, res) => {
-try {
-    
-} catch (error) {
-    
-}
-}
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const updatePlaylist = (req, res) => {
-try {
-    
-} catch (error) {
-    
-}
-}
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const addTrack = (req, res) => {
-try {
-    
-} catch (error) {
-    
-}
-}
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const removeTrack = (req, res) => {
-try {
-    
-} catch (error) {
-    
-}
-}
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const deletePlaylist = (req, res) => {
-try {
-    
-} catch (error) {
-    
-}
-}
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+};
