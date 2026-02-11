5df61be3a7b97a1a6c821ce20f5bc7453d54df32
Julien08 <julienalex08@gmail.com>
2026-02-11 11:51:27 -0500

    playlist detail

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import authRouter from "./auth.js";
 import usersRouter from "./users.js";
 import tracksRouter from "./tracks.js";
+import playlistRouter from "./playlist.js";
 
 const router = Router();
 
 router.use("/auth", authRouter);
 router.use("/users", usersRouter);
 router.use("/tracks", tracksRouter);
+router.use("/playlist", playlistRouter);
 
 export default router;
