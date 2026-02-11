1de76654055e4063fc3385503397479e7ad21ea6
RCasares316 <richiecasares@gmail.com>
2026-02-11 10:04:49 -0500

    tracks functionality updated

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,31 +1,10 @@
 import { Router } from "express";
-import axios from "axios";
-import { getAccessToken } from "../utils/soundCloudAuth.js";
+import verifyToken from "../middleware/verify-token.js";
+import * as controllers from "../controllers/tracks.js";
 
 const router = Router();
 
-// This receives the track id from your DB
-router.get("/:trackId", async (req, res) => {
-  try {
-    const token = await getAccessToken();
-
-    const soundcloudUrl = `https://api.soundcloud.com/tracks/${req.params.trackId}/preview`;
-
-    const scRes = await axios.get(soundcloudUrl, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      responseType: "stream",
-    });
-
-    // Important headers so browser knows this is audio
-    res.setHeader("Content-Type", "audio/mpeg");
-
-    scRes.data.pipe(res);
-  } catch (err) {
-    console.error(err.response?.data || err.message);
-    res.status(500).send("Error streaming audio");
-  }
-});
+router.get("/", verifyToken, controllers.getTracks);
+router.get("/:trackId", verifyToken, controllers.getTrack);
 
 export default router;
